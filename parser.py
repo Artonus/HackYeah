f089da2f2460003e82165c31e4979463d072a569
amtlib <grzegorz.andrzej.pach@gmail.com>
2019-09-14 17:25:13 +0200

    Add file support

--- parser.py
+++ parser.py
@@ -8,28 +8,38 @@ class Parser:
     pause_re = re.compile(r'^PAUSE (\d{1,5})$', re.IGNORECASE)
     base_re = re.compile(r'^BASE$', re.IGNORECASE)
 
+    def parse_file(file_path):
+        with open(file_path, 'r') as f:
+            commands = f.readlines()
+            for command in commands:
+                __parse_user_input(command)
+                
+    def __parse_user_input(user_input):
+        if self.clockwise_re.search(user_input):
+                degrees = int(self.clockwise_re.search(user_input).group(1))
+                print('clockwise {} degrees'.format(degrees))
+        elif self.counterclockwise_re.search(user_input):
+            degrees = int(self.counterclockwise_re.search(user_input).group(1))
+            print('counterclockwise {} degrees'.format(degrees))
+        elif self.stop_re.search(user_input):
+            print('bye')
+            break
+        elif self.speed_re.search(user_input):
+            speed = int(self.speed_re.search(user_input).group(1))
+            print('set speed {}'.format(speed))
+        elif self.pause_re.search(user_input):
+            pause = int(self.pause_re.search(user_input).group(1))
+            print('pause {}'.format(pause))
+        elif self.base_re.search(user_input):
+            print('base')
+        else:
+            print('Unknown command')
+
     def start(self):
         while True:
             user_input = input('> ')
-            if self.clockwise_re.search(user_input):
-                degrees = int(self.clockwise_re.search(user_input).group(1))
-                print('clockwise {} degrees'.format(degrees))
-            elif self.counterclockwise_re.search(user_input):
-                degrees = int(self.counterclockwise_re.search(user_input).group(1))
-                print('counterclockwise {} degrees'.format(degrees))
-            elif self.stop_re.search(user_input):
-                print('bye')
-                break
-            elif self.speed_re.search(user_input):
-                speed = int(self.speed_re.search(user_input).group(1))
-                print('set speed {}'.format(speed))
-            elif self.pause_re.search(user_input):
-                pause = int(self.pause_re.search(user_input).group(1))
-                print('pause {}'.format(pause))
-            elif self.base_re.search(user_input):
-                print('base')
-            else:
-                print('Unknown command')
+            self.__parse_user_input(user_input)
+    
 if __name__ == '__main__':
     parser = Parser()
     parser.start()
