a78d39183f005f114b3b40e02043275c5148934a
Bartosz Bratus <artonus1@gmail.com>
2019-09-14 22:36:49 +0200

    Poprawa błędów kompilacji

--- algorithms.py
+++ algorithms.py
@@ -18,14 +18,18 @@ def getMatrixFrom4Points(point1,point2,point3,point4):
 def what_do(refpoint1,refpoint2,point):         #refpointy to stałe punkty, point ma być ściśle zmienny
     a = ( refpoint2[1] - refpoint1[1] ) / ( refpoint2[0] - refpoint1[0] )
     b = refpoint2[1] - ( a * refpoint2[0] )
-    isRaising = a > 0 ? true : false
+    if a>0:
+        isRaising = True
+        pass
+    else:
+        isRaising = False
     y = ( a * point[0] ) + b
     if point[1] > y:
         return true                             #czyli krecimy zgodnie z ruchem wskazówek zegara!
-    else
+    else:
         return false                            #czyli że nie
 
 img = cv2.imread("kolo.png")
 rows,cols,ch = img.shape
 # dst = cv2.warpAffine(img, getMatrixFrom4Points((830,1359),(1820,203),(1002,1207),(2673,1178)), (img.shape[1], img.shape[0]))
-M = getMatrixFrom4Points((830,1359),(1820,203),(1002,1207),(2673,1178));
+M = getMatrixFrom4Points((830,1359),(1820,203),(1002,1207),(2673,1178))
