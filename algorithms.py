7b876a747246700e0749cd8251559e85746f7685
AndrzejPodo <andrzejpodobinski2@gmail.com>
2019-09-14 23:16:51 +0200

    moze już działa

--- algorithms.py
+++ algorithms.py
@@ -7,10 +7,13 @@ def findAlfaOnThreePoints(pointA, pointB, pointC):
     v2 = [pointC[0]-pointA[0], pointC[1]-pointA[1]]
     len1 = math.sqrt(v1[0]*v1[0]+v1[1]*v1[1])
     len2 = math.sqrt(v2[0]*v2[0]+v2[1]*v2[1])
+    dot = numpy.dot(v1,v2)
     print(len2*len2)
     print(numpy.dot(v1,v2)) 
     print(numpy.dot(v1,v2)/(len1*len2))
-    return math.acos(numpy.dot(v1,v2)/(len1*len2))
+    if(dot > 0):
+        return math.acos(numpy.dot(v1,v2)/(len1*len2))
+    return -math.acos(numpy.dot(v1,v2)/(len1*len2))
 
 # def getMatrixFrom4Points(point1,point2,point3,point4):
 #     pts1 = np.float32([point1,point2,point3,point4])
@@ -31,4 +34,4 @@ def what_do(refpoint1,refpoint2,point):         #refpointy to stałe punkty, poi
     else:
         return False                            #czyli że nie
 
-# print(findAlfaOnThreePoints((400,300),(400,0),(100,300)))
+print(findAlfaOnThreePoints((400,300),(400,0),(100,300)))
