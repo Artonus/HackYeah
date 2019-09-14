31e19b9192eea71d232b10cd2b941d75f42b8fa5
Bartosz Bratus <artonus1@gmail.com>
2019-09-14 20:51:53 +0200

    Poprawki w strukturze klasy ConnectionManager

--- ConectionManager.py
+++ ConectionManager.py
@@ -1,43 +1,59 @@
 import paho.mqtt.client as mqtt
-import paho.mqtt.publish as mqtt
 
-# The callback for when the client receives a CONNACK response from the server.
 class Connection():
-    def on_connect(self, client, userdata, flags, rc):
+
+    _client = mqtt.Client("P1")
+
+    @staticmethod
+    def on_connectEvent(client, userdata, flags, rc):
+        if rc == 0:
+            print("Conn working")
+            pass
+        else:
+            print("Conn not working")
+            pass
         print("Connected with result code " + str(rc))
         # Subscribing in on_connect() means that if we lose the connection and
-        # reconnect then subscriptions will be renewed.
-        
-    pass
-
-    def on_message(client, userdata, msg):
-        print(msg.topic+" "+str(msg.payload))
-
-    def __init__(self):
-        broker_address="192.168.0.1"
-        port = 1883
-        self._topic = "mosquitto_pub"
-        self._client = mqtt.Client(self._topic)
-        self._client.on_connect = on_connect
-        self._client.on_message = on_message
-        self._client.connect(broker_address, port, 60)
-
-        # Blocking call that processes network traffic, dispatches callbacks and
-        # handles reconnecting.
-        # Other loop*() functions are available that give a threaded interface and a
-        # manual interface.
-        self._client.loop_forever()        
+        # reconnect then subscriptions will be renewed.        
         pass
+
+    @staticmethod
+    def on_messageEvent(client, userdata, msg):
+        print(msg.topic+" "+str(msg.payload))
+        pass
+    
     # move type: move or freq
-    def sendCommand(self, moveType, value):
-        #obosolete
-        #message = "mosquitto_pub -h 192.168.0.1 -t {mov} -m {val}".format(fre = value, mov = moveType)
+    def sendCommand(self, moveType, value):        
+        # obosolete:
+        # message = "mosquitto_pub -h 192.168.0.1 -t {mov} -m {val}".format(fre = value, mov = moveType)        
         self._client.subscribe(moveType)
         self._client.publish(moveType, value)
+        print("Command send: ", moveType, value)
         
         pass
-    def disconnect(self):
-        if self._client.isconnected():
-            self._client.disconnect()
-            pass        
+    # Należy zawsze wykonać przed zamknięciem programu
+    def disconnect(self):        
+        self._client.disconnect()
+        self._client.loop_stop()        
         pass
+
+    def on_disconnect(client, userdata, rc):
+        client.loop_stop()
+        print("Disconected")
+        pass
+
+    def __init__(self):
+        try:
+            broker_address="mqtt.eclipse.org" #"192.168.0.1"
+            port = 1883            
+            self._client.on_connect = self.on_connectEvent
+            self._client.on_message = self.on_messageEvent
+            self._client.connect(broker_address, port, 60)
+            
+            self._client.loop_start()
+            pass
+        except Exception as ex:
+            print(ex)
+            pass             
+        pass
+    pass
