c3a9885cd1ec2ecc516e7a0bb7d57a6b019c1650
amtlib <grzegorz.andrzej.pach@gmail.com>
2019-09-14 17:00:14 +0200

    As class

--- parser.py
+++ parser.py
@@ -1,29 +1,38 @@
 import re
 
+class Parser:
+    clockwise_re = re.compile(r'^CLOCKWISE (\d{1,3})$', re.IGNORECASE)
+    counterclockwise_re = re.compile(r'^COUNTERCLOCKWISE (\d{1,3})$', re.IGNORECASE)
+    stop_re = re.compile(r'^STOP$', re.IGNORECASE)
+    speed_re = re.compile(r'^SPEED (\d{1,2})$', re.IGNORECASE)
+    pause_re = re.compile(r'^PAUSE (\d{1,5})$', re.IGNORECASE)
+    base_re = re.compile(r'^BASE$', re.IGNORECASE)
+    exit_re = re.compile(r'EXIT', re.IGNORECASE)
 
-clockwise_re = re.compile(r'^CLOCKWISE (\d{1,3})$', re.IGNORECASE)
-counterclockwise_re = re.compile(r'^COUNTERCLOCKWISE (\d{1,3})$', re.IGNORECASE)
-stop_re = re.compile(r'^STOP$', re.IGNORECASE)
-speed_re = re.compile(r'^SPEED (\d{1,2})$', re.IGNORECASE)
-pause_re = re.compile(r'^PAUSE (\d{1,5})$', re.IGNORECASE)
-base_re = re.compile(r'^BASE$', re.IGNORECASE)
-while True:
-    user_input = input('> ')
-    if clockwise_re.search(user_input):
-        degrees = int(clockwise_re.search(user_input).group(1))
-        print('clockwise {} degrees'.format(degrees))
-    elif counterclockwise_re.search(user_input):
-        degrees = int(counterclockwise_re.search(user_input).group(1))
-        print('counterclockwise {} degrees'.format(degrees))
-    elif stop_re.search(user_input):
-        print('stop')
-    elif speed_re.search(user_input):
-        speed = int(speed_re.search(user_input).group(1))
-        print('set speed {}'.format(speed))
-    elif pause_re.search(user_input):
-        pause = int(pause_re.search(user_input).group(1))
-        print('pause {}'.format(pause))
-    elif base_re.search(user_input):
-        print('base')
-    else:
-        print('Unknown command')
+    def start(self):
+        while True:
+            user_input = input('> ')
+            if self.clockwise_re.search(user_input):
+                degrees = int(self.clockwise_re.search(user_input).group(1))
+                print('clockwise {} degrees'.format(degrees))
+            elif self.counterclockwise_re.search(user_input):
+                degrees = int(self.counterclockwise_re.search(user_input).group(1))
+                print('counterclockwise {} degrees'.format(degrees))
+            elif self.stop_re.search(user_input):
+                print('stop')
+            elif self.speed_re.search(user_input):
+                speed = int(self.speed_re.search(user_input).group(1))
+                print('set speed {}'.format(speed))
+            elif self.pause_re.search(user_input):
+                pause = int(self.pause_re.search(user_input).group(1))
+                print('pause {}'.format(pause))
+            elif self.base_re.search(user_input):
+                print('base')
+            elif  self.exit_re.search(user_input):
+                print('bye')
+                break
+            else:
+                print('Unknown command')
+if __name__ == '__main__':
+    parser = Parser()
+    parser.start()
