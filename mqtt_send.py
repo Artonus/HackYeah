6b98aca6607302825bb5d3103c5437a89140baf7
Witold Serwatka <witold.serwatka@pm.me>
2019-09-14 17:00:15 +0200

    Podstawa pliku do komunikacji z serwerem Node

---
+++ mqtt_send.py
@@ -0,0 +1,15 @@
+Import paho.mqtt.client as mqtt
+
+client = mqtt.Client() # client_id randomly generated, apparently
+
+client.connect(test.mosquitto.org) #zamienic na IP
+
+argNum = len(sys.argv)
+n=0 #witamy na WDI
+
+if argNum % 2 != 0:
+    print("Odd number of parameters!")
+
+while n < argNum:
+    client.publish(sys.argv[n],sys.argv[n+1])
+    n += 2
