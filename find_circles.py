f6e5adb23d132bd7d2765b10ee81fac67e48c68b
PanMaciek <koziejka.com@gmail.com>
2019-09-15 01:31:26 +0200

    controller interface

--- find_circles.py
+++ find_circles.py
@@ -20,7 +20,6 @@ SPINNING_MASK = ([142, 181, 0], [255, 255, 255])
 
 source = './data/3.mp4'
 # source = 'rstp://'
-video = cv2.VideoCapture(source)
 
 cv2.namedWindow("bosch", cv2.WINDOW_NORMAL)
 cv2.resizeWindow('frame', 600, 600)
@@ -61,48 +60,48 @@ def read_point(image, mask):
         points.append(point)
     return points
 
-
-ret, frame = video.read()
-TOP_POINT = read_point(frame, TOP_MASK)[0]
-CENTER_POINT = read_point(frame, CENTER_MASK)[0]
-
-
-
-
-while(video.isOpened()):
+def run(controller):
+    video = cv2.VideoCapture(source)
     ret, frame = video.read()
-    if not ret:
-        video.set(cv2.CAP_PROP_POS_FRAMES, 0)
-        continue
+    TOP_POINT = read_point(frame, TOP_MASK)[0]
+    CENTER_POINT = read_point(frame, CENTER_MASK)[0]
 
-    img = apply_mask(frame, SPINNING_MASK)
+    while(video.isOpened()):
+        ret, frame = video.read()
+        if not ret:
+            video.set(cv2.CAP_PROP_POS_FRAMES, 0)
+            continue
 
-    gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
-    gray = cv2.GaussianBlur(gray, (5, 5), 0)
+        img = apply_mask(frame, SPINNING_MASK)
 
-    thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
-    thresh = cv2.erode(thresh, None, iterations=2)
-    thresh = cv2.dilate(thresh, None, iterations=2)
+        gray = cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
+        gray = cv2.GaussianBlur(gray, (5, 5), 0)
 
-    cnts = cv2.findContours(
-        thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
-    cnts = imutils.grab_contours(cnts)
+        thresh = cv2.threshold(gray, 45, 255, cv2.THRESH_BINARY)[1]
+        thresh = cv2.erode(thresh, None, iterations=2)
+        thresh = cv2.dilate(thresh, None, iterations=2)
 
-    ROTATING_POINT = mean1D(cnts[0])
-    RED = (0, 0, 255)
-    cv2.circle(frame, ROTATING_POINT, 5, RED, 6)
+        cnts = cv2.findContours(
+            thresh.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+        cnts = imutils.grab_contours(cnts)
 
-    cv2.line(frame, ROTATING_POINT, TOP_POINT, RED, 3)
-    cv2.line(frame, ROTATING_POINT, CENTER_POINT, RED, 3)
-    cv2.line(frame, TOP_POINT, CENTER_POINT, RED, 3)
+        ROTATING_POINT = mean1D(cnts[0])
+        RED = (0, 0, 255)
+        cv2.circle(frame, ROTATING_POINT, 5, RED, 6)
 
-    phi = findAlfaOnThreePoints(CENTER_POINT, TOP_POINT, ROTATING_POINT)
-    x_offset = int(math.cos(phi) * 100)
-    y_offset = int(math.sin(phi) * 100)
-    cv2.circle(frame, (CENTER_POINT[0] + x_offset,
-                       CENTER_POINT[1] + y_offset), 5, (0, 255, 0), 6)
+        cv2.line(frame, ROTATING_POINT, TOP_POINT, RED, 3)
+        cv2.line(frame, ROTATING_POINT, CENTER_POINT, RED, 3)
+        cv2.line(frame, TOP_POINT, CENTER_POINT, RED, 3)
 
-    cv2.imshow('bosch', frame)
+        phi = findAlfaOnThreePoints(CENTER_POINT, TOP_POINT, ROTATING_POINT)
+        x_offset = int(math.cos(phi) * 100)
+        y_offset = int(math.sin(phi) * 100)
+        cv2.circle(frame, (CENTER_POINT[0] + x_offset,
+                        CENTER_POINT[1] + y_offset), 5, (0, 255, 0), 6)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imshow('bosch', frame)
+
+        controller(phi)
+
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
---
+++ main.py
@@ -0,0 +1,7 @@
+from find_circles import run
+
+def controller(angle):
+    #TODO: IMPLEMENT PID
+    print(angle)
+
+run(controller)
